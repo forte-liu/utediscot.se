5aacb4c728bd5ca91a1936c46368d8da1aa87164
Niclas Olofsson <n@niclasolofsson.se>
2012-07-20 20:19:30 +0200

    Restructured and prettified stuff

---
+++ static/js/slider.js
@@ -0,0 +1,9 @@
+$(function() {
+    $('#start_images').nivoSlider({
+        effect: 'boxRandom',
+        pauseTime: 8000, // How long each slide will show
+        animSpeed: 1000,
+        directionNav: false, // Next & Prev navigation
+        controlNav: false // 1,2,3... navigation
+    });
+});
